42b4e60d772155fcd76245390f4a18feb9b33e32
Stef Verheijen <dev@stef.be>
2023-04-17 21:44:56 +0200

    Draggable/Resizable UAE window. Fixes #3

--- _script/ui/components/uae.js
+++ _script/ui/components/uae.js
@@ -8,6 +8,9 @@ import {COMMAND} from "../../enum.js";
 let UAE = function(){
     let me = {};
     let container;
+    let currentTranslate = [0,0];
+    let currentSize = [720,588];
+    let currentPosition = [(window.innerWidth-currentSize[0])>>1,(window.innerHeight-currentSize[1])>>1];
 
     me.preview = function(){
         window.getUaeContent = function(){
@@ -20,7 +23,7 @@ let UAE = function(){
                             if (root && root.files){
                                 let pictureFile = root.files.find(f=>f.name === "picture.iff");
                                 if (pictureFile){
-                                    let image = Generate.iff();
+                                    let image = Generate.iff(32);
                                     if (image){
                                         image.arrayBuffer().then(buffer=>{
                                             Adf.deleteFileAtSector(pictureFile.sector);
@@ -45,11 +48,41 @@ let UAE = function(){
         }
         if (container) container.remove();
         container = $(".uae",
-            $(".caption","Amiga Preview",$(".close",{
+            $(".caption.handle",{
+                onDragStart:()=>{
+                  container.classList.add("dragging");
+                },
+                onDrag:(x,y)=>{
+                    x += currentPosition[0];
+                    y += currentPosition[1];
+                    currentTranslate = [x,y];
+                    container.style.transform = "translate("+x+"px,"+y+"px)";
+                },
+                onDragEnd:()=>{
+                    container.classList.remove("dragging");
+                    currentPosition = [currentTranslate[0],currentTranslate[1]];
+                }},"Amiga Preview",$(".close",{
                 onclick:()=>{
                     me.hide();
                     },info:"Close Amiga Preview"},"x")
-            ));
+            ),$(".resizer.handle",{
+                onDragStart:()=>{
+                    container.classList.add("dragging");
+                },
+                onDrag:(x,y)=>{
+                    x += currentSize[0];
+                    y += currentSize[1];
+                    container.style.width = x+"px";
+                    container.style.height = y+"px";
+                },
+                onDragEnd:()=>{
+                    container.classList.remove("dragging");
+                    currentSize = [container.clientWidth,container.clientHeight];
+                }
+            }));
+        container.style.transform = "translate("+currentPosition[0]+"px,"+currentPosition[1]+"px)";
+        container.style.width = currentSize[0]+"px";
+        container.style.height = currentSize[1]+"px";
         let iframe = $("iframe",{
             src:"uae/index.html"
         })
