a1cbdc4eabcb405f61d4590ff56d41f8704620cf
Steffest <dev@stef.be>
2023-07-09 16:55:23 +0200

    More efficient LZW Decoder for GIF files

--- _script/fileformats/gif.js
+++ _script/fileformats/gif.js
@@ -124,7 +124,7 @@ const GIF = (()=>{
                         for (let i = 0; i < size; i++) lzwData.push(file.readUbyte());
                     } while (size > 0);
 
-                    frame.pixels = lzwDecode2(lzwData, block.lzwMinCodeSize);
+                    frame.pixels =  lzwDecode(lzwData, block.lzwMinCodeSize, frame.width*frame.height);
                     if (!frame.palette) frame.palette = img.palette;
                     if (img.transparentColorFlag){
                         frame.transparentColorIndex = img.transparentColorIndex;
@@ -208,77 +208,104 @@ const GIF = (()=>{
     }
 
 
-
-
-    var lzwDecode2 = function(data,minCodeSize) {
-        // this part is taken from https://github.com/shachaf/jsgif/blob/master/gif.js
+    function lzwDecode(data, minCodeSize, pixelCount){
+        // This function is taken from https://github.com/matt-way/gifuct-js
         // available under MIT license
-        // TODO: optimize
 
-        var pos = 0;
+        const MAX_STACK_SIZE = 4096;
+        const nullCode = -1;
+        const npix = pixelCount;
+        let available, clear, code_mask, code_size, end_of_information, in_code, old_code, code, i, data_size;
 
-        var readCode = function(size) {
-            var code = 0;
-            for (var i = 0; i < size; i++) {
-                if (data[pos >> 3] & (1 << (pos & 7))) {
-                    code |= 1 << i;
-                }
-                pos++;
-            }
-            return code;
-        };
+        const dstPixels = new Array(pixelCount);
+        const prefix = new Array(MAX_STACK_SIZE);
+        const suffix = new Array(MAX_STACK_SIZE);
+        const pixelStack = new Array(MAX_STACK_SIZE + 1);
 
-        var output = [];
-
-        var clearCode = 1 << minCodeSize;
-        var eoiCode = clearCode + 1;
-
-        var codeSize = minCodeSize + 1;
-
-        var dict = [];
-
-        var clear = function() {
-            dict = [];
-            codeSize = minCodeSize + 1;
-            for (var i = 0; i < clearCode; i++) {
-                dict[i] = [i];
-            }
-            dict[clearCode] = [];
-            dict[eoiCode] = null;
-
-        };
-
-        var code;
-        var last;
-
-        while (true) {
-            last = code;
-            code = readCode(codeSize);
-
-            if (code === clearCode) {
-                clear();
-                continue;
-            }
-            if (code === eoiCode) break;
-
-            if (code < dict.length) {
-                if (last !== clearCode) {
-                    dict.push(dict[last].concat(dict[code][0]));
-                }
-            } else {
-                if (code !== dict.length) throw new Error('Invalid LZW code.');
-                dict.push(dict[last].concat(dict[last][0]));
-            }
-            output.push.apply(output, dict[code]);
-
-            if (dict.length === (1 << codeSize) && codeSize < 12) {
-                codeSize++;
-            }
+        // Initialize GIF data stream decoder.
+        data_size = minCodeSize;
+        clear = 1 << data_size;
+        end_of_information = clear + 1;
+        available = clear + 2;
+        old_code = nullCode;
+        code_size = data_size + 1;
+        code_mask = (1 << code_size) - 1;
+        for (code = 0; code < clear; code++) {
+            prefix[code] = 0;
+            suffix[code] = code;
         }
 
-        return output;
-    };
+        // Decode GIF pixel stream.
+        let datum, bits, count, first, top, pi, bi
+        datum = bits = count = first = top = pi = bi = 0;
+        for (i = 0; i < npix; ) {
+            if (top === 0) {
+                if (bits < code_size) {
+                    // get the next byte
+                    datum += data[bi] << bits;
+                    bits += 8;
+                    bi++;
+                    continue;
+                }
+                // Get the next code.
+                code = datum & code_mask;
+                datum >>= code_size;
+                bits -= code_size;
+                // Interpret the code
+                if (code > available || code == end_of_information) break;
+                if (code == clear) {
+                    // Reset decoder.
+                    code_size = data_size + 1;
+                    code_mask = (1 << code_size) - 1;
+                    available = clear + 2;
+                    old_code = nullCode;
+                    continue;
+                }
+                if (old_code == nullCode) {
+                    pixelStack[top++] = suffix[code];
+                    old_code = code;
+                    first = code;
+                    continue;
+                }
+                in_code = code;
+                if (code == available) {
+                    pixelStack[top++] = first;
+                    code = old_code;
+                }
+                while (code > clear) {
+                    pixelStack[top++] = suffix[code];
+                    code = prefix[code];
+                }
 
+                first = suffix[code] & 0xff;
+                pixelStack[top++] = first;
+
+                // add a new string to the table, but only if space is available
+                // if not, just continue with current table until a clear code is found
+                // (deferred clear code implementation as per GIF spec)
+                if (available < MAX_STACK_SIZE) {
+                    prefix[available] = old_code;
+                    suffix[available] = first;
+                    available++;
+                    if ((available & code_mask) === 0 && available < MAX_STACK_SIZE) {
+                        code_size++;
+                        code_mask += available;
+                    }
+                }
+                old_code = in_code;
+            }
+            // Pop a pixel off the pixel stack.
+            top--;
+            dstPixels[pi++] = pixelStack[top];
+            i++;
+        }
+
+        for (i = pi; i < npix; i++) {
+            dstPixels[i] = 0; // clear missing pixels
+        }
+
+        return dstPixels;
+    }
 
     return me;
 })();
